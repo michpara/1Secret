6a7895f19a609b1ca7987d05546379cd5fedfcef
Raymond Chen <Sizbei@users.noreply.github.com>
2021-03-05 18:23:39 -0500

    Update linkpage.js

--- front/src/component/linkpage.js
+++ front/src/component/linkpage.js
@@ -2,40 +2,56 @@ import { NavLink } from "react-router-dom";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
 import Timer from "react-compound-timer";
-import React from "react";
 
 
-let number = 25;
-let value = 100000000;
-let init = 10000000000;
 
 const LinkPage = (props) => {
+  const currTime = (new Date).getTime();
+  let remainingTime = (props.data.expiry_time * 1000) - currTime;
+  const url = `http://localhost:3000/receivedlink/${props.data.id}`
   return (
     <div>
       <p className="mute-text"  style={{ "font-weight": "700" }}> 
-        <Timer     initialTime={value}
+        <Timer     initialTime={remainingTime}
           direction="backward" lastUnit="m">
           <Timer.Minutes /> 
         </Timer> min left </p>
         
-      <ProgressBar animated now={value} />
-      <br></br>
+
+        <ProgressBar animated now={} />
+        <br></br>
+        {props.data.expiry_time === 0 ? 
+        <React.Fragment> 
+       
+        <h2 style={{ "font-weight": "700" }}>
+          Your 1Secret Link is expired
+        </h2>
+        <textarea class="input"></textarea>  
+        </React.Fragment> :
+      <React.Fragment> 
+       
       <h2 style={{ "font-weight": "700" }}>
         Your 1Secret Link is ready to share
       </h2>
-      <textarea class="input"></textarea>
-      <p className="expiration-time">This link will expire in <span> </span>
-        <Timer     initialTime={value}
+      <textarea class="input">{url}</textarea>  
+      </React.Fragment>}
+      
+      {props.data.expiry_time === 0 ? <p className="expiration-time">To resend your password, please generate another link.
+        </p> : <p className="expiration-time">This link will expire in <span> </span>
+        <Timer     initialTime={remainingTime}
           direction="backward" lastUnit="m">
           
           <Timer.Minutes /> minutes <span> </span>
           <Timer.Seconds /> seconds 
         
         </Timer>
-        </p> 
+        </p> }
+ 
       <br></br>
        
+      
        
        
       
