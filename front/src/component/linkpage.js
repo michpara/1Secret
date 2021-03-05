1ff9b40f84174803c5a8503559097ceb3939ea2a
Kevin Xu <karkevin@gmail.com>
2021-03-05 18:52:20 -0500

    progress

--- front/src/component/linkpage.js
+++ front/src/component/linkpage.js
@@ -9,6 +9,9 @@ import Row from "react-bootstrap/Row";
 const LinkPage = (props) => {
   const [copied, setCopyStatus] = useState(false);
 
+  const creationTime = props.data.creation_time; 
+  const expiryTime = props.data.expiry_time;
+
   const currTime = new Date().getTime();
   const curr = new Intl.DateTimeFormat("en-US", {
     hour: 'numeric',
@@ -21,68 +24,69 @@ const LinkPage = (props) => {
     navigator.clipboard.writeText(url);
     setCopyStatus(true);
   };
-  return (
-    <div>
-      <p className="mute-text" style={{ "font-weight": "700" }}>
-        <Timer initialTime={remainingTime} direction="backward" lastUnit="m">
-          <Timer.Minutes />
-        </Timer>{" "}
-        min left{" "}
-      </p>
 
-      <ProgressBar animated now={remainingTime} />
-      <br></br>
-      {props.data.expiry_time === 0 ? (
-        <React.Fragment>
-          <h2 style={{ "font-weight": "700" }}>
-            Your 1Secret Link is ready to share
-          </h2>
-          <input class="input"></input>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Row>
-            <input value={url}></input>
-            <div
-              onClick={() => {
-                copyCodeToClipboard();
-              }}
-              class="copy"
-            >
-              <FontAwesomeIcon icon={faCopy} />
-            </div>
-          </Row>
-        </React.Fragment>
-      )}
-
-      {props.data.expiry_time === 0 ? (
-        <p className="expiration-time">
-          To resend your password, please generate another link.
-        </p>
-      ) : (
-        <p className="expiration-time">
-          This link will expire in <span> </span>
+    return (
+      <div>
+        <p className="mute-text" style={{ "font-weight": "700" }}>
           <Timer initialTime={remainingTime} direction="backward" lastUnit="m">
-            <Timer.Minutes /> minutes <span> </span>
-            <Timer.Seconds /> seconds
-          </Timer>
+            <Timer.Minutes />
+          </Timer>{" "}
+          min left{" "}
         </p>
-      )}
-      <br></br>
-
-      <br></br>
-      <NavLink
-        exact
-        activeClassName="current"
-        style={{ "font-family": "Raleway" }}
-        to="/"
-      >
-        Generate Another Link <FontAwesomeIcon icon={faChevronRight} />
-      </NavLink>
-
-      {copied ? <div className="copied">Copied at {curr}</div> : null}
-    </div>
-  );
+  
+        <ProgressBar animated now={((expiryTime - (currTime/1000))/(expiryTime - creationTime)) * 100} />
+        <br></br>
+        {props.data.expiry_time === 0 ? (
+          <React.Fragment>
+            <h2 style={{ "font-weight": "700" }}>
+              Your 1Secret Link is ready to share
+            </h2>
+            <input class="input"></input>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Row>
+              <input value={url}></input>
+              <div
+                onClick={() => {
+                  copyCodeToClipboard();
+                }}
+                class="copy"
+              >
+                <FontAwesomeIcon icon={faCopy} />
+              </div>
+            </Row>
+          </React.Fragment>
+        )}
+  
+        {props.data.expiry_time === 0 ? (
+          <p className="expiration-time">
+            To resend your password, please generate another link.
+          </p>
+        ) : (
+          <p className="expiration-time">
+            This link will expire in <span> </span>
+            <Timer initialTime={remainingTime} direction="backward" lastUnit="m">
+              <Timer.Minutes /> minutes <span> </span>
+              <Timer.Seconds /> seconds
+            </Timer>
+          </p>
+        )}
+        <br></br>
+  
+        <br></br>
+        <NavLink
+          exact
+          activeClassName="current"
+          style={{ "font-family": "Raleway" }}
+          to="/"
+        >
+          Generate Another Link <FontAwesomeIcon icon={faChevronRight} />
+        </NavLink>
+  
+        {copied ? <div className="copied">Copied at {curr}</div> : null}
+      </div>
+    );
 };
 
 export default LinkPage;
